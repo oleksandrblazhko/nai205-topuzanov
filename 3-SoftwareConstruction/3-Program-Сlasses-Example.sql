461bd01b988887696ec9e71c08773bbf632d360d
BogdanTopuzanov <79829528+BogdanNopuzanov@users.noreply.github.com>
2022-11-21 18:50:20 +0200

    Update 3-Program-Сlasses-Example.sql

--- 3-SoftwareConstruction/3-Program-Сlasses-Example.sql
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example.sql
@@ -1,50 +1,61 @@
-CREATE OR REPLACE TYPE Sculpture UNDER Info and Video (
- id_Sculpture SERIAL PRIMARY KEY,
-Name varchar,
-DopInfo varchar(1500),
-      OVERRIDING MEMBER PROCEDURE display 
-);
-CREATE OR REPLACE TYPE Info (
- d_informacion SERIAL PRIMARY KEY,
-Дата_започаткування date,
-Hanr varchar,
-Difficulty int,
-Type_Sculpture char,
-Hinfo char(1500),
-      OVERRIDING MEMBER PROCEDURE display 
-);
-CREATE OR REPLACE TYPE Video AS OBJECT (
-  id_video SERIAL PRIMARY KEY,
-Name varchar,
-File_size int,
-Format char,
-Dlina int,
-      MEMBER PROCEDURE display
+
+/* Створення об'єктного типу (клас) My_Sulpture,
+при створенні потрібен термінальний символ /
+як при реєстрації звичайного програмного модуля PL/SQL
+*/
+-- DROP TYPE Location FORCE;
+CREATE OR REPLACE TYPE My_Sulpture AS OBJECT (
+    SName VARCHAR(15),       -- назва скульптури
+    DopInfo VARCHAR(15),      --информация
+    Difficulty  NUMBER -- складність скульптурирование
+    /* конструктор екземплярів об'єктів класів.
+       Вхідні параметри:
+	   1) SName - назва 
+	   Вихідний параметр - екземпляр обєкту класу
+	*/
+    CONSTRUCTOR FUNCTION My_Sulpture (SName VARCHAR)
+        RETURN SELF AS RESULT,
+	/* Процедура зміни значення атрибутів */
+	MEMBER PROCEDURE set_SName( SName VARCHAR, DopInfo VARCHAR,Difficulty  NUMBER),
+    /* Функції отримання значень атрибутів */
+	MEMBER FUNCTION get_Difficulty RETURN NUMBER,
+	MEMBER FUNCTION get_SName RETURN VARCHAR,
+ MEMBER FUNCTION get_DopInfo RETURN VARCHAR,
+    /* Процедура виводу на екран значень атрибутів */
+    MEMBER PROCEDURE display
 ); 
+/
+-- show errors;
+
+
+/* Створення об'єктного типу (класу) KUser */
 CREATE OR REPLACE TYPE KUser AS OBJECT (
-  id_korystuvach SERIAL PRIMARY KEY,
-Fname varchar,
-Sname varchar,
-Age int,
-Masterstvo int,
-Phone_number char,
-  
-      MEMBER PROCEDURE display
+ Kname  VARCHAR(15),      -- имя/фамлиия   
+ Masterstvo NUMBER(2),        -- мастерство   
+ Phone   VARCHAR(15),         -- телефон
+	/* Процедура виводу на екран значень атрибутів */
+    MEMBER PROCEDURE display
 ); 
-CREATE OR REPLACE TYPE AppSculpture UNDER Sculpture AS OBJECT (
-  id_dodatok SERIAL PRIMARY KEY,
-Name varchar,
-Type_ varchar,
-Rozmer float,
-MinHaracteristiki  varchar(300),
-Soderzhanie  varchar(300),
-      MEMBER PROCEDURE display
-  CONSTRUCTOR FUNCTION Video(Name varchar)
-          RETURN SELF AS RESULT,
-        /* Функції отримання значень атрибутів */
-    MEMBER FUNCTION get_Informacion RETURN NUMBER,
-    MEMBER FUNCTION get_Video RETURN VARCHAR,
-    MEMBER FUNCTION get_price RETURN NUMBER,
-  /* Процедура виводу на екран значень атрибутів */
-      MEMBER PROCEDURE display
-); 
+/
+
+/* Створення об'єктного типу (класу) Video */
+CREATE OR REPLACE TYPE Video AS OBJECT (
+    Vname VARCHAR(10), -- ім'я видео 
+    Vsize INT, -- размер фалйла
+    format VARCHAR(10), -- формат видео
+    MEMBER PROCEDURE display
+);
+/
+/* Створення типу для зберігання списку співробітників - 
+"Колекція екземплярів об`єктів класу Employee */
+CREATE TYPE KUser_List IS TABLE OF KUser;
+/
+
+/* Cтворення об'єктного типу (класу) Manager, 
+що успадковує об'єктний тип Employee */
+CREATE OR REPLACE TYPE Manager UNDER KUser (
+    manage KUser_List, -- список співробітників, якими керує менеджер
+	/* перевизначення методу-процедури класу KUser */
+    OVERRIDING MEMBER PROCEDURE display 
+);
+/
