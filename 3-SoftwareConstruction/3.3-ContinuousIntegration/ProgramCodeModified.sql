0e8149583379a85faf1932adb20650df6e36892c
BogdanTopuzanov <79829528+BogdanNopuzanov@users.noreply.github.com>
2022-11-26 15:11:31 +0200

    Update ProgramCodeModified.sql

--- 3-SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
+++ 3-SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
@@ -1,8 +1,7 @@
-/* Створення об'єктного типу (класу) My_user */
-CREATE TABLE My_user(
-	user_id INT, -- id користувача
-	full_name VARCHAR(100), -- ПІБ
-	phone VARCHAR(20), -- номер телефону
-	pseudonym VARCHAR(100), -- псевдонім
+CREATE TABLE KUser (
+	 user_id INT, -- id користувача
+	 Kname  VARCHAR(15),      -- имя/фамлиия   
+	 Masterstvo NUMBER(2),        -- мастерство   
+	 Phone   VARCHAR(15),         -- телефон
     	NOT FINAL MEMBER PROCEDURE display
 ) NOT FINAL 
